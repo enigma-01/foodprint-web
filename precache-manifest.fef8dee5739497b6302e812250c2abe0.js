966fed59404cfbe830a623aff1f3804f2ba31f57
Kevin Pierce <kevinpierce08@gmail.com>
2020-06-15 13:05:46 -0400

    Updates

---
+++ precache-manifest.fef8dee5739497b6302e812250c2abe0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b120dae08f1007ce6fd093ba943192",
+    "url": "/foodprint-web/index.html"
+  },
+  {
+    "revision": "f3a5523251d40d096472",
+    "url": "/foodprint-web/static/css/main.b98bb2c8.chunk.css"
+  },
+  {
+    "revision": "a12c6e330c3b0fac4ac2",
+    "url": "/foodprint-web/static/js/2.f56d8713.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/foodprint-web/static/js/2.f56d8713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3a5523251d40d096472",
+    "url": "/foodprint-web/static/js/main.026b5716.chunk.js"
+  },
+  {
+    "revision": "f2218c32fdf34c3a9faa",
+    "url": "/foodprint-web/static/js/runtime-main.144cc939.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/foodprint-web/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/foodprint-web/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/foodprint-web/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/foodprint-web/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "43dd5b7a3d277362d5e801e5353e3a01",
+    "url": "/foodprint-web/static/media/Montserrat-Thin.43dd5b7a.ttf"
+  },
+  {
+    "revision": "10a5b071548379871a331ddae24e289f",
+    "url": "/foodprint-web/static/media/check.10a5b071.svg"
+  },
+  {
+    "revision": "659e0fc254f7abcecc44919cf45d597f",
+    "url": "/foodprint-web/static/media/contact-page-background.659e0fc2.jpg"
+  },
+  {
+    "revision": "0409273f09b4f051d602d58d5835fd4f",
+    "url": "/foodprint-web/static/media/homepage-header-image.0409273f.png"
+  },
+  {
+    "revision": "18b94da65047f31a62d9f370bc3957f7",
+    "url": "/foodprint-web/static/media/newBackground.18b94da6.png"
+  },
+  {
+    "revision": "efcea9b2e23fafe76e6a055313c8d99b",
+    "url": "/foodprint-web/static/media/new_location.efcea9b2.png"
+  },
+  {
+    "revision": "8a019ec87ab516dc6e39314461ac09de",
+    "url": "/foodprint-web/static/media/other-header-img.8a019ec8.png"
+  }
+]);
