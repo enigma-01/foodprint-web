ef99c3af9104f75e7ffaf474219ac33803f5c812
Kevin Pierce <kevinpierce08@gmail.com>
2020-08-20 23:56:08 -0400

    Revised styling for footer

--- src/compenents/FOOTER/index.jsx
+++ src/compenents/FOOTER/index.jsx
@@ -4,32 +4,45 @@ import { NavLink } from "react-router-dom";
 
 const StyledDiv = styled.div`
   width: 100vw;
-  height: 50px;
-  background-color: #4E4F50;
+  height: 30px;
+  background-color: #4e4f50;
 `;
 
 const StyledNavLink = styled(NavLink)`
-    color: #f6b26b;
-    margin: 5px;
-    padding: 5px;
-    text-decoration: none;
-    width: wrap-content;
-    font-size: 20px;
-    font-weight: 500;
+  color: #f6b26b;
+  margin: 5px;
+  padding: 5px;
+  text-decoration: none;
+  width: wrap-content;
+  font-size: 20px;
+  font-weight: 300;
 
-    :hover {
+  :hover {
     opacity: 0.6;
     transition: opacity 0.3s;
-    }
-`
+  }
+`;
+
+const StyledText = styled.p`
+  font-size: 12px;
+  color: #FFFFFF;
+  font-weight: 300;
+  margin:0 0 0 10px;
+`;
 
 const FooterDiv = () => {
   return (
-    <StyledDiv>
-      <StyledNavLink to="/privacy-policy">Privacy Policy</StyledNavLink>
-      <StyledNavLink to="/terms-of-service">Terms of Service</StyledNavLink>
-    </StyledDiv>
-  )
+    <div>   
+        <StyledDiv>
+            <StyledNavLink to="/privacy-policy">Privacy Policy</StyledNavLink>
+            <StyledNavLink to="/terms-of-service">Terms of Service</StyledNavLink>
+        </StyledDiv>
+        <StyledDiv>
+            <StyledText>©Kevin Pierce. All Rights Reserved.</StyledText>
+        </StyledDiv>
+    </div>
+    
+  );
 };
 
 export default FooterDiv;
